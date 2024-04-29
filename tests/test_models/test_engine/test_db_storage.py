8defe1cd7af75b5a8c40baf2d0a49e2125736289
Olaitan34 <fatkayus@gmail.com>
2024-04-29 01:42:43 +0100

    code

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -67,18 +67,6 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-        # Assuming db_session and cls are properly defined for testing
-    def test_db_storage_get_existing_object(self):
-        # Assuming id of existing object is 1
-        obj = DBStorage().get(cls, 1)
-        assert obj is not None
-
-    def test_db_storage_get_non_existing_object(self):
-        # Assuming id of non-existing object is -1
-        obj = DBStorage().get(cls, -1)
-        assert obj is None
-
-
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
@@ -98,3 +86,24 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    def test_get_db(self):
+        """ Tests method for obtaining an instance db storage"""
+        dic = {"name": "Cundinamarca"}
+        instance = State(**dic)
+        storage.new(instance)
+        storage.save()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
+
+    def test_count(self):
+        """ Tests count method db storage """
+        dic = {"name": "Vecindad"}
+        state = State(**dic)
+        storage.new(state)
+        dic = {"name": "Mexico", "state_id": state.id}
+        city = City(**dic)
+        storage.new(city)
+        storage.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)
